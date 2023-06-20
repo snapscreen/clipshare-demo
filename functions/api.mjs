d17be3a2515fccb38eda2c932bb8711de87ff94b
Matthias Grieder <matthias.grieder@snapscreen.com>
2023-06-20 11:55:31 +0200

    update api

--- functions/api.mjs
+++ functions/api.mjs
@@ -1,11 +1,10 @@
-// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
-
-import express, { Router } from 'express';
+import express from 'express';
+import { Router } from 'express';
 import serverless from 'serverless-http';
 
 const api = express();
-
 const router = Router();
+
 router.get('/hello', (req, res) => res.send('Hello World!'));
 
 api.use('/api/', router);
