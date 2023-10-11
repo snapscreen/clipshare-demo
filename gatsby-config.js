13504ccebf25a9b92db52defa48591b7a9312947
Matthias Grieder <matthias.grieder@snapscreen.com>
2023-10-11 21:34:50 +0200

    update theme_color

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
         short_name: `ClipShare`,
         start_url: `/`,
         background_color: `#00172e`,
-        theme_color: `#f7a600`,
+        theme_color: `#e98300`,
         display: `minimal-ui`,
         icon: `src/images/ClipShare_Icon.svg`, // This path is relative to the root of the site.
       }
